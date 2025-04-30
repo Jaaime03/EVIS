c1cbdefe9e99cc43590d6dde1eaf1ba8751b7c80
aliciaguzman <alicia.guzman@alumnos.upm.es>
2025-04-30 17:50:26 +0200

    modificación EjercicioAlumno
    Se ha modificado la class EjercicioAlumno añadiendo de fk alumno

--- core/models.py
+++ core/models.py
@@ -22,9 +22,22 @@ class EnunciadoEjercicio(models.Model):
         return self.nombre_ejercicio
 
 
+class Alumno(models.Model):
+    id_alumno = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=200)
+    matricula = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    dni = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellidos}"
+    
+    
 class EjercicioAlumno(models.Model):
     id_ejercicio_alum = models.AutoField(primary_key=True)
     enunciado = models.ForeignKey(EnunciadoEjercicio, on_delete=models.PROTECT, related_name='ejercicios_alumno')
+    alumno = models.ForeignKey(Alumno, on_delete=models.PROTECT, related_name='ejercicios_alumno')
     url_foto_ejerc = models.URLField(max_length=500)
     ocr_imag_to_text = models.TextField()
     correcto_ocr = models.BooleanField(default=False)
@@ -43,18 +56,6 @@ class Error(models.Model):
 
     def __str__(self):
         return f"Error {self.id_error}"
-
-
-class Alumno(models.Model):
-    id_alumno = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100)
-    apellidos = models.CharField(max_length=200)
-    matricula = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
-    dni = models.IntegerField(unique=True)
-
-    def __str__(self):
-        return f"{self.nombre} {self.apellidos}"
 
 
 class AlumnoErrorEjercicio(models.Model):
