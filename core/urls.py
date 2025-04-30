2f3f35214cd7408529cd98598e1d592cef09c483
aliciaguzman <alicia.guzman@alumnos.upm.es>
2025-04-30 18:11:21 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EjercicioViewSet
+from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-router = DefaultRouter()
-router.register(r'ejercicios', EjercicioViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('crear_examen/', views.crear_examen, name='crear_examen'),
+    path('crear_enunciado_ejercicio/', views.crear_enunciado_ejercicio, name='crear_enunciado_ejercicio'),
+    path('subir_imagenes/', views.subir_imagenes_ejercicio, name='subir_imagenes'),
+    path('procesar-ocr/', views.procesar_ocr_api, name='procesar_ocr'),
+    path('procesar-correccion/', views.procesar_correccion_api, name='procesar_correccion'),
 ]
