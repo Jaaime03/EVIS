73a5e42a8af35e2d8441ae08018734b51f001618
aliciaguzman <alicia.guzman@alumnos.upm.es>
2025-05-07 10:37:18 +0200

    actualización core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
+from .views import SubirEjercicioView, ErrorInformacionView, ActualizarPenalizacionProfView, ListaImagenesYTextosOCR, CorreccionOCRView, ErroresAlumnoListView
 
+# Routers
 router = DefaultRouter()
 router.register(r'examenes', views.ExamenViewSet)
 router.register(r'enunciados', views.EnunciadoEjercicioViewSet)
@@ -12,20 +14,19 @@ router.register(r'errores', views.ErrorViewSet)
 router.register(r'alumnos', views.AlumnoViewSet)
 router.register(r'alumno-errores', views.AlumnoErrorEjercicioViewSet)
 
+# URL patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-]
 
-urlpatterns += [
+    # JWT Authentication
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
 
-urlpatterns += [
-    path('crear_examen/', views.crear_examen, name='crear_examen'),
-    path('crear_enunciado_ejercicio/', views.crear_enunciado_ejercicio, name='crear_enunciado_ejercicio'),
-    path('subir_imagenes/', views.subir_imagenes_ejercicio, name='subir_imagenes'),
-    path('procesar-ocr/', views.procesar_ocr_api, name='procesar_ocr'),
-    path('procesar-correccion/', views.procesar_correccion_api, name='procesar_correccion'),
+    # Custom endpoint
+    path('crear-ejercicio/', SubirEjercicioView.as_view(), name='crear-ejercicio'),
+    path('errores-informacion/', ErrorInformacionView.as_view(), name='errores-informacion'),
+    path('errores/<int:pk>/actualizar-penalizacion-prof/', ActualizarPenalizacionProfView.as_view(), name='actualizar-penalizacion-prof'),
+    path('imagenes-ocr/', ListaImagenesYTextosOCR.as_view(), name='lista-imagenes-ocr'),
+    path('ejercicio-alumno/<int:pk>/correccion-ocr/', CorreccionOCRView.as_view(), name='correccion-ocr'),
 ]
